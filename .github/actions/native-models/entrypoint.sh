922f25fdf831a20fcd5b73d7d8c827452d5ed4a1
David Lawes <david.lawes@guardian.co.uk>
2023-03-27 14:24:47 +0100

    generate swift models on release

--- .github/actions/native-models/entrypoint.sh
+++ .github/actions/native-models/entrypoint.sh
@@ -12,30 +12,32 @@ git config --global user.email '<>'
 # fixes error: "fatal: detected dubious ownership in repository at '/github/workspace'"
 git config --global --add safe.directory /github/workspace
 
-# We get the branch name from an input
-export BRANCH=$2
+# We get the version from an input
+export VERSION=$2
 
-# Clone repo
+# Clone repos
 git clone https://github.com/guardian/mobile-apps-api-models.git
+git clone https://github.com/guardian/mapi-models-swift.git
 
-# We will generate a new commit on the target branch
-echo "Checking out branch: $BRANCH"
-cd mobile-apps-api-models
-git checkout $BRANCH
-
-# Generate Swift (output will be in gen-swift folder)
-echo "Generating Swift code..."
-thrift --gen swift -r models/src/main/thrift/collection.thrift
+# Generating the swift models is part of the release flow
+# We should only be releasing after merging to the main branch
+echo "Generating Swift models..."
+thrift --gen swift -r mobile-apps-api-models/models/src/main/thrift/collection.thrift
 
 # Hopefully -f is a first-time-only thing
-rm -f Sources/Collection/*.swift
-cp gen-swift/* Sources/Collection
+rm -f mapi-models-swift/Sources/Collection/*.swift
+cp gen-swift/* mapi-models-swift/Sources/Collection
 
+# Commit and tag latest swift models
+cd mapi-models-swift
 git add Sources/Collection/*.swift
 
 # --allow-empty because we don't want to fail if model is exactly the same (?)
-git commit --allow-empty -m "Update swift models"
+git commit --allow-empty -m "Update swift models based on https://github.com/guardian/mobile-apps-api-models/releases/tag/$VERSION"
+git tag $VERSION
 
-git push origin HEAD
+# Push the commit and tag
+git push -u origin HEAD
+git push --tags
 
 echo "Finished!"
